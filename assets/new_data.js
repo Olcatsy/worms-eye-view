8cb2243a30ddbf105e06656d74c6b79cf90a6021
Olga Filiushkina <olga.filiushkina@gmail.com>
2020-11-08 01:47:54 -0500

    started adding new object assets and adding them to the data file

---
+++ assets/new_data.js
@@ -0,0 +1,407 @@
+const data = {
+  scene_a: {
+    letter: 'a',
+    layers: {
+      layer01: {
+        layerNum: '01',
+        allItemsFound: false,
+        interactive_items: [
+          {
+            alt: "A broken smartphone with a shattered flickering screen",
+            digSitePosition: {},
+            id: "item_a_01_01",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/objects/object_a_01.png',
+            copy: "A broken smartphone with a shattered flickering screen",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "A blue surgical mask",
+            digSitePosition: {},
+            id: "item_a_01_02",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/objects/object_a_02.png',
+            copy: "A blue surgical mask",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "A cross section of a blueish grey cable with an orange center. The conductor forms a lattice. Inside, the electric energy is shifting in rainbow streaks that cast violet light. ",
+            digSitePosition: {},
+            id: "item_a_01_03",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/objects/object_a_03.png',
+            copy: "A cross section of a blueish grey cable with an orange center. The conductor forms a lattice. Inside, the electric energy is shifting in rainbow streaks that cast violet light.",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "A book with yellowing pages and a tattered grey cover with brown-green stains.It has 'Diary' written on it. The title is upside down",
+            digSitePosition: {},
+            id: "item_a_01_04",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/objects/object_a_04.png',
+            copy: "A book with yellowing pages and a tattered grey cover with brown-green stains.It has 'Diary' written on it. The title is upside down",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "A bright green microchip with a metal component that is shimmering with silver energy",
+            digSitePosition: {},
+            id: "item_a_01_05",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/objects/object_a_05.png',
+            copy: "A bright green microchip with a metal component that is shimmering with silver energy",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "A bright lime green microchip with golden connectors and a black processor unit shimmering with silver energy",
+            digSitePosition: {},
+            id: "item_a_01_06",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/objects/object_a_06.png',
+            copy: "A bright lime green microchip with golden connectors and a black processor unit shimmering with silver energy",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "",
+            digSitePosition: {},
+            id: "item_a_01_07",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/objects/object_a_07.png',
+            copy: "",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "A shaggy paint brush with a blue handle and brown bristles",
+            digSitePosition: {},
+            id: "item_a_01_08",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/objects/object_a_08.png',
+            copy: "A shaggy paint brush with a blue handle and brown bristles",
+            // inventory_position: str (related to a grid?)
+          },
+        ]
+      },
+      layer02: {
+        layerNum: '02',
+        allItemsFound: false,
+        interactive_items: [
+          {
+            alt: "a wilted brown leaf",
+            digSitePosition: {},
+            id: "item_a_02_01",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/objects/object_a_02.png',
+            copy: "A wilted brown leaf",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "a burning $1000000 Hell Bank Note",
+            digSitePosition: {},
+            id: "item_a_02_02",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/objects/object_a_05.gif',
+            copy: "a burning $1000000 Hell Bank Note",
+            // inventory_position: str (related to a grid?)
+          },
+        ]
+      },
+      layer03: {
+        layerNum: '03',
+        allItemsFound: false,
+        interactive_items: [
+          {
+            alt: "a butterfly emerging from a cocoon",
+            digSitePosition: {},
+            id: "item_a_03_01",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/objects/object_a_04.gif',
+            copy: "a butterfly emerging from a cocoon",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "an broken smartphone with flickering screen",
+            digSitePosition: {},
+            id: "item_a_03_02",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/objects/object_a_06.gif',
+            copy: "an broken smartphone with flickering screen",
+            // inventory_position: str (related to a grid?)
+          },
+        ]
+      },
+    },
+  },
+  scene_b: {
+    letter: 'b',
+    layers: {
+      layer01: {
+        layerNum: '01',
+        allItemsFound: false,
+        interactive_items: [
+          {
+            alt: "A bunch of seven bananas, their color is shifting between yellow and green",
+            digSitePosition: {},
+            id: "item_b_01_07",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_b_01.png',
+            copy: "A bunch of seven bananas, their color is shifting between yellow and green",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "a burning $1000000 Hell Bank Note (joss paper) with the portrait of the Jade Emperor surrounded by rice plants depicted in the middle",
+            digSitePosition: {},
+            id: "item_b_02_08",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_b_08.gif',
+            copy: "a burning $1000000 Hell Bank Note (joss paper) with the portrait of the Jade Emperor surrounded by rice plants depicted in the middle",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "A yellow Asian pear wrapped in white mesh",
+            digSitePosition: {},
+            id: "item_b_01_10",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_b_10.png',
+            copy: "A yellow Asian pear wrapped in white mesh",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "Two bananas are lying next to each other, their color shifting from green to yellow to red",
+            digSitePosition: {},
+            id: "item_b_01_11",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_b_11.png',
+            copy: "Two bananas are lying next to each other, their color shifting from green to yellow to red",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "A chunk of concrete with pale blue glaze on one side",
+            digSitePosition: {},
+            id: "item_b_01_15",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_b_15.png',
+            copy: "A chunk of concrete with pale blue glaze on one side",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "A cream colored rock speckled with blue and brown. The rock still retains a bit of pale blue glaze.",
+            digSitePosition: {},
+            id: "item_b_01_16",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_b_16.png',
+            copy: "A cream colored rock speckled with blue and brown. The rock still retains a bit of pale blue glaze.",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "A large irregular slab of grey concrete ",
+            digSitePosition: {},
+            id: "item_b_01_18",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_b_18.png',
+            copy: "A large irregular slab of grey concrete ",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "Two plant sprouts with small green leaves and their root system attached",
+            digSitePosition: {},
+            id: "item_b_01_19",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_b_19.png',
+            copy: "Two plant sprouts with small green leaves and their root system attached",
+            // inventory_position: str (related to a grid?)
+          },
+        ]
+      },
+      layer02: {
+        layerNum: '02',
+        allItemsFound: false,
+        interactive_items: [
+          {
+            alt: "a wilted brown leaf",
+            digSitePosition: {},
+            id: "item_b_02_01",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_a_02.png',
+            copy: "A wilted brown leaf",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "a burning $1000000 Hell Bank Note",
+            digSitePosition: {},
+            id: "item_b_02_02",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_a_05.gif',
+            copy: "a burning $1000000 Hell Bank Note",
+            // inventory_position: str (related to a grid?)
+          },
+        ]
+      },
+      layer03: {
+        layerNum: '03',
+        allItemsFound: false,
+        interactive_items: [
+          {
+            alt: "a butterfly emerging from a cocoon",
+            digSitePosition: {},
+            id: "item_b_03_01",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_a_04.gif',
+            copy: "a butterfly emerging from a cocoon",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "an broken smartphone with flickering screen",
+            digSitePosition: {},
+            id: "item_b_03_02",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_a_06.gif',
+            copy: "an broken smartphone with flickering screen",
+            // inventory_position: str (related to a grid?)
+          },
+        ]
+      },
+    },
+  },
+  scene_c: {
+    letter: 'c',
+    layers: {
+      layer01: {
+        layerNum: '01',
+        allItemsFound: false,
+        interactive_items: [
+          {
+            alt: "An elongated blue rock with yellow specks",
+            digSitePosition: {},
+            id: "item_c_01_22",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_c_22.png',
+            copy: "An elongated blue rock with yellow specks",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "A wilted brown leaf with signs of rotting",
+            digSitePosition: {},
+            id: "item_c_01_23",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_c_23.png',
+            copy: "A wilted brown leaf with signs of rotting",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "A long green grass blade forming two loops",
+            digSitePosition: {},
+            id: "item_c_01_38",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_c_38.png',
+            copy: "A long green grass blade forming two loops",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "A rounded green rock",
+            digSitePosition: {},
+            id: "item_c_01_41",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_c_41.png',
+            copy: "A rounded green rock",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "A rounded green rock",
+            digSitePosition: {},
+            id: "item_c_01_41",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_c_41.png',
+            copy: "A rounded green rock",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "An irregularly shaped meteorite streak with black, orange and red. At the core there is a flickering flame that can be peeked through holes.",
+            digSitePosition: {},
+            id: "item_c_01_46",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_c_46.png',
+            copy: "An irregularly shaped meteorite streak with black, orange and red. At the core there is a flickering flame that can be peeked through holes.",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "Two halves of an oval blue-green shell moving towards each other, then separating. The inside of the shell is pink",
+            digSitePosition: {},
+            id: "item_c_01_53",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_c_53.png',
+            copy: "Two halves of an oval blue-green shell moving towards each other, then separating. The inside of the shell is pink",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "A crawling white slug with black horns, grey spots and black tail. ",
+            digSitePosition: {},
+            id: "item_c_01_56",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_c_56.png',
+            copy: "A crawling white slug with black horns, grey spots and black tail.",
+            // inventory_position: str (related to a grid?)
+          },
+          {
+            alt: "A light yellow rock with speckles of blue and brown ",
+            digSitePosition: {},
+            id: "item_c_01_57",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_c_57.png',
+            copy: "A light yellow rock with speckles of blue and brown ",
+            // inventory_position: str (related to a grid?)
+          },
+        ],
+      },
+      layer02: {
+        layerNum: '02',
+        allItemsFound: false,
+        interactive_items: [
+          {
+            alt: "A wilted brown leaf with signs of rotting",
+            digSitePosition: {},
+            id: "item_c_01_23",
+            inInventory: false,
+            isTransparent: false,
+            src: './assets/object_c_23.png',
+            copy: "A wilted brown leaf with signs of rotting",
+            // inventory_position: str (related to a grid?)
+          },
+        ],
+      },
+    },
+  },
+}
+
+export default data;
