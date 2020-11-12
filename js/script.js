f2bd1b73ae4937c26ef437b39603c1128d96fed1
Olga Filiushkina <olga.filiushkina@gmail.com>
2020-11-12 17:18:13 -0500

    tweaked brush sensitivity

--- js/script.js
+++ js/script.js
@@ -132,7 +132,7 @@ const app = {
     
       // if checkTransparency returns 'true' item's isTransparent to true
       // The second argument is the percent of pixels scratched off in the area
-      if (app.checkTransparency(pixelsData, 70)) {
+      if (app.checkTransparency(pixelsData, 30)) {
         helper.updateProperty(dataArr, i, 'isTransparent', true);
         item.classList.add('found-item');
       };
